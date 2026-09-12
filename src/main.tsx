0fd9bbb05621086cbbc70ddbc74b2fb719c01cb7
MR <mrdigitalvip@gmail.com>
2026-09-12 19:47:08 -0300

    fix: guard app boot with runtime boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { registerLookSpaceServiceWorker } from './pwa/register'
+import { RuntimeBoundary } from './ui/System/RuntimeBoundary'
 import './styles/index.css'
 import './styles/mission.css'
 import './styles/cinematic.css'
@@ -9,8 +10,18 @@ import './styles/pwa.css'
 
 registerLookSpaceServiceWorker()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('LookSpace root element was not found')
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <RuntimeBoundary>
+      <App />
+    </RuntimeBoundary>
   </React.StrictMode>,
 )
+
+requestAnimationFrame(() => {
+  ;(window as Window & { __LOOKSPACE_BOOTED__?: boolean }).__LOOKSPACE_BOOTED__ = true
+  document.documentElement.dataset.lookspaceBooted = 'true'
+})
