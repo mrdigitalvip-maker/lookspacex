17fdd805c34ce45506c92020eb5f83e526d3b593
MR <mrdigitalvip@gmail.com>
2026-09-12 19:24:12 -0300

    feat: enable PWA runtime registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { registerLookSpaceServiceWorker } from './pwa/register'
 import './styles/index.css'
 import './styles/mission.css'
 import './styles/cinematic.css'
+
+registerLookSpaceServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
